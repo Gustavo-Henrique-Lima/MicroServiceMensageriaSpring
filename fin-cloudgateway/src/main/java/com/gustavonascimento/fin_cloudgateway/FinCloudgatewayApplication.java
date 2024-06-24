6ce42ddc6aba7ac9ca7d8b4f7d1d90a8ad959f50
Gustavo Henrique <gustavohgustavo@hotmail.com>
2024-06-23 21:53:12 -0300

    Refactor: Refactoring the Route Configuration Bean.

--- fin-cloudgateway/src/main/java/com/gustavonascimento/fin_cloudgateway/FinCloudgatewayApplication.java
+++ fin-cloudgateway/src/main/java/com/gustavonascimento/fin_cloudgateway/FinCloudgatewayApplication.java
@@ -3,9 +3,6 @@ package com.gustavonascimento.fin_cloudgateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -15,9 +12,4 @@ public class FinCloudgatewayApplication {
 		SpringApplication.run(FinCloudgatewayApplication.class, args);
 	}
 	
-	@Bean
-	public RouteLocator routes(RouteLocatorBuilder builder) {
-		return builder.routes().route(r->r.path("/clients/**").uri("lb://fin-clientes")).build();
-	}
-
 }
